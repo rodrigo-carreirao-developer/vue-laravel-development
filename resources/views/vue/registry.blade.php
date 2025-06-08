ded4c725d6ecea6a11fcc23d70e8411fee6255d5
Rodrigo Carreirão <rodrigo.carreirao@gmail.com>
2025-06-08 18:59:31 -0300

    Adding blade files that holds vue startup pages

---
+++ resources/views/vue/registry.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>User Registry</title>
+    @vite('resources/js/app.js')
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-gray-100">
+    <div id="app">
+        <user-registry-component></user-registry-component>
+    </div>
+</body>
+</html>
---
+++ resources/views/vue/user.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel + Vue + Pinia + user</title>
+    @vite('resources/js/app.js')
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-gray-100">
+    <div id="app">
+        <main-header></main-header>
+    </div>
+
+</body>
+</html>
